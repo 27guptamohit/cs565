cc44ab2abf6f3d598231c151159123bc6058d4d5
Alan Zhou <alanzyt311@gmail.com>
2023-05-03 15:58:10 -0500

    update experience and optional email

--- src/FrontEnd/Pages/ParticipantSide/00_entrance_screen.tsx
+++ src/FrontEnd/Pages/ParticipantSide/00_entrance_screen.tsx
@@ -15,28 +15,47 @@ const ParticipantEntraceScreen = () => {
     event.preventDefault();
     const form = event.target as HTMLFormElement;
     const email = form.email.value;
-    const experience = form.experience.value;
+    const experience = (form.querySelector('input[name="musicExperience"]:checked') as HTMLInputElement)?.value;
   
-    axios.post(`${ENDPOINT}/api/users`, {
-      email: email,
-      experience: experience
-    })
-    .then((response) => {
-      const userId = response.data.result._id;
-      navigate('/activity', { state: { userId } });
-    })
-    .catch((error) => {
-      setErrorMessage(error.response.data.message);
-    });
+    if (!email) {
+        // If email is empty, only post experience
+        axios.post(`${ENDPOINT}/api/users`, {
+          experience: experience
+        })
+        .then((response) => {
+          const userId = response.data.result._id;
+          navigate('/activity', { state: { userId } });
+        })
+        .catch((error) => {
+          setErrorMessage(error.response.data.message);
+        });
+      } else {
+        // If email is not empty, post both email and experience
+        axios.post(`${ENDPOINT}/api/users`, {
+          email: email,
+          experience: experience
+        })
+        .then((response) => {
+          const userId = response.data.result._id;
+          navigate('/activity', { state: { userId } });
+        })
+        .catch((error) => {
+          setErrorMessage(error.response.data.message);
+        });
+      }
   }
 
   return (
     <div className="container d-flex flex-column justify-content-center align-items-center">
         <div className="welcome-message">Welcome to the Music Sheet Identification activity!</div>
-        <img src = "/notes/img7.png" alt = "music"></img>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="email">Your Email</label>
+        <img src = "/notes/img7.png" alt = "music" style={{ width: '50%', height: 'auto' }}></img>
+        <div>About the study:</div>
+        <div>xxxx</div>
+
+      <form onSubmit={handleSubmit} style={{ textAlign: 'center' }}>
+        <div className="form-group" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <h4 style={{ textAlign: 'center' }}>Your Email - Optional, for Gift Card Raffel only</h4>
+          {/* <label htmlFor="email">Your Email - Optional, only for Gift Card Raffel</label> */}
           <input
             type="email"
             className="form-control"
@@ -47,7 +66,7 @@ const ParticipantEntraceScreen = () => {
             style={{ width: '300px' }}
           />
         </div>
-        <div className="form-group">
+        {/* <div className="form-group">
           <label htmlFor="experience">Your Music Experience</label>
           <input
             type="text"
@@ -58,6 +77,37 @@ const ParticipantEntraceScreen = () => {
             onChange={(event) => setExperience(event.target.value)}
             style={{ width: '300px' }}
           />
+        </div> */}
+
+        <div style={{ textAlign: 'center' }}>
+        <h4>Your Experience of Writing/Reading Music</h4>
+        <form>
+            <div>
+                <label>
+                <input type="radio" name="musicExperience" value="Zero experience" defaultChecked />
+                Zero experience
+                </label>
+            </div>
+            <div>
+                <label>
+                <input type="radio" name="musicExperience" value="Some experience" />
+                Some experience
+                </label>
+            </div>
+            <div>
+                <label>
+                <input type="radio" name="musicExperience" value="Actively working with music" />
+                Actively working with music
+                </label>
+            </div>
+            <div>
+                <label>
+                <input type="radio" name="musicExperience" value="Prefer not to answer" />
+                Prefer not to answer
+                </label>
+            </div>
+            </form>
+
         </div>
         <button type="submit" className="btn btn-primary" style={{ marginTop: '20px' }}>Submit</button>
         {errorMessage && <div className="error-message">{errorMessage}</div>}
